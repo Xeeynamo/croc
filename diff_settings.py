54c5fdc41a967d5654aedd60b4d2b218b6e59c9b
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-03-31 15:52:57 +0100

    Update diff to have parity with sotn-decomp

--- diff_settings.py
+++ diff_settings.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
+import os
+
+
+def add_custom_arguments(parser):
+    parser.add_argument(
+        "--version",
+        default="sled00038",
+        dest="version",
+        help="Decide what version of the game to use (list of builds in disks/README)",
+    )
+    parser.add_argument(
+        "--overlay",
+        default="croc",
+        dest="overlay",
+        help="Defines which overlay to use for the diff",
+    )
+
 def apply(config, args):
+    version = args.version or os.getenv("VERSION") or "sled00038"
+    name = args.overlay or "croc"
     config["arch"] = "mipsel"
-    config['baseimg'] = f'CROC.EXE'
-    config['myimg'] = f'build/croc.exe'
-    config['mapfile'] = f'build/croc.map'
-    config['source_directories'] = [f'src/croc', 'include', f'asm/croc']
+    config['baseimg'] = f'disks/{version}/' + (f"/{name}.PRG").upper()
+    config['myimg'] = f'build/' + (f"/{name}.exe").upper()
+    config['mapfile'] = f'build/{name}.map'
+    config['source_directories'] = [f'src/{name}', 'include', f'asm/{name}']
